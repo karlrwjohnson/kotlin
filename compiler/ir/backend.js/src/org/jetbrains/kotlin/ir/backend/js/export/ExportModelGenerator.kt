dd13915a1511714c80e7752ed2b5075e10afd6b6
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-06-21 17:08:24 +0200

    chore: complete strict implicit export feature.

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/export/ExportModelGenerator.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/export/ExportModelGenerator.kt
@@ -237,7 +237,7 @@ class ExportModelGenerator(val context: JsIrBackendContext, val generateNamespac
             .filter { it !is ExportedType.ErrorType }
 
         val name = klass.getExportedIdentifier()
-        val (_, nestedClasses) = exportClassDeclarations(klass)
+        val (members, nestedClasses) = exportClassDeclarations(klass)
         return ExportedClass(
             name = name,
             isInterface = true,
@@ -245,7 +245,7 @@ class ExportModelGenerator(val context: JsIrBackendContext, val generateNamespac
             superClass = null,
             superInterfaces = superInterfaces,
             typeParameters = typeParameters,
-            members = emptyList(),
+            members = members,
             nestedClasses = nestedClasses,
             ir = klass
         )
@@ -311,9 +311,11 @@ class ExportModelGenerator(val context: JsIrBackendContext, val generateNamespac
     ): ExportedClassDeclarationsInfo {
         val members = mutableListOf<ExportedDeclaration>()
         val nestedClasses = mutableListOf<ExportedClass>()
+        val isImplicitlyExportedClass = klass.isJsImplicitExport()
 
         for (declaration in klass.declarations) {
             val candidate = getExportCandidate(declaration) ?: continue
+            if (isImplicitlyExportedClass && candidate !is IrClass) continue
             if (!shouldDeclarationBeExportedImplicitlyOrExplicitly(candidate, context)) continue
 
             val processingResult = specialProcessing(candidate)
@@ -368,13 +370,13 @@ class ExportModelGenerator(val context: JsIrBackendContext, val generateNamespac
     }
 
     private fun IrClass.shouldNotBeImplemented(): Boolean {
-        return isInterface && !isExternal
+        return isInterface && !isExternal || isJsImplicitExport()
     }
 
     private fun IrClass.shouldContainImplementationOfMagicProperty(superTypes: Iterable<IrType>): Boolean {
         return !isExternal && superTypes.any {
             val superClass = it.classOrNull?.owner ?: return@any false
-            superClass.isInterface && superClass.isExported(context)
+            superClass.isInterface && superClass.isExported(context) || superClass.isJsImplicitExport()
         }
     }
 
@@ -396,11 +398,13 @@ class ExportModelGenerator(val context: JsIrBackendContext, val generateNamespac
     }
 
     private fun MutableList<ExportedDeclaration>.addMagicPropertyForInterfaceImplementation(klass: IrClass, superTypes: Iterable<IrType>) {
-        val superTypesToInheritanceMagicProperty = superTypes.filter { it.shouldAddMagicPropertyOfSuper(context) }
+        val allSuperTypesWithMagicProperty = superTypes.filter { it.shouldAddMagicPropertyOfSuper(context) }
 
-        if (superTypesToInheritanceMagicProperty.isEmpty()) return
+        if (allSuperTypesWithMagicProperty.isEmpty()) {
+            return
+        }
 
-        val intersectionOfTypes = superTypesToInheritanceMagicProperty
+        val intersectionOfTypes = allSuperTypesWithMagicProperty
             .map { ExportedType.PropertyType(exportType(it), ExportedType.LiteralType.StringLiteralType(magicPropertyName)) }
             .reduce(ExportedType::IntersectionType)
             .let { if (klass.shouldNotBeImplemented()) ExportedType.IntersectionType(klass.generateTagType(), it) else it }
@@ -427,10 +431,10 @@ class ExportModelGenerator(val context: JsIrBackendContext, val generateNamespac
     }
 
     private fun IrClass.isOwnMagicPropertyAdded(context: JsIrBackendContext): Boolean {
+        if (isJsImplicitExport()) return true
         if (!isExported(context)) return false
         return isInterface && !isExternal || superTypes.any {
-            val klass = it.classOrNull?.owner ?: return@any false
-            klass.isExported(context) && klass.isOwnMagicPropertyAdded(context)
+            it.classOrNull?.owner?.isOwnMagicPropertyAdded(context) == true
         }
     }
 
