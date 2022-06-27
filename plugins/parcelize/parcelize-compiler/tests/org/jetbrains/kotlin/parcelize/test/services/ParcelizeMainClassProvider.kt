f4572322edf6dc3e72d9ca907cb24ccb439fe35b
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2022-06-27 18:53:49 +0200

    Use cache-redirector for downloading in parcelize tests
    There's a flaky failures when
    ~/.m2/repository/org/robolectric/android-all/5.0.2_r3-robolectric-r0/android-all-5.0.2_r3-robolectric-r0.jar
    is partially downloaded during tests execution.
    This led to Parcelize tests failure with:
    
    Caused by: java.util.zip.ZipException: invalid LOC header (bad signature)
     at java.util.zip.ZipFile.read(Native Method)
     at java.util.zip.ZipFile.access$1400(ZipFile.java:60)
     at java.util.zip.ZipFile$ZipFileInputStream.read(ZipFile.java:734)
     at java.util.zip.ZipFile$ZipFileInflaterInputStream.fill(ZipFile.java:434)
     at java.util.zip.InflaterInputStream.read(InflaterInputStream.java:158)
     at java.io.FilterInputStream.read(FilterInputStream.java:133)
     at java.io.FilterInputStream.read(FilterInputStream.java:107)
     at org.robolectric.util.Util.copy(Util.java:21)
     at org.robolectric.util.Util.readBytes(Util.java:38)
     at org.robolectric.internal.bytecode.SandboxClassLoader.getByteCode(SandboxClassLoader.java:166)
    ... 36 more
    
    Option used was found at https://github.com/robolectric/robolectric/blob/7697d1217c5eb0dee454004efe60ba219f398255/plugins/maven-dependency-resolver/src/main/java/org/robolectric/MavenRoboSettings.java#L19

--- plugins/parcelize/parcelize-compiler/tests/org/jetbrains/kotlin/parcelize/test/services/ParcelizeMainClassProvider.kt
+++ plugins/parcelize/parcelize-compiler/tests/org/jetbrains/kotlin/parcelize/test/services/ParcelizeMainClassProvider.kt
@@ -13,6 +13,7 @@ import org.junit.runner.JUnitCore
 class ParcelizeMainClassProvider(testServices: TestServices) : JvmBoxMainClassProvider(testServices) {
     override fun getMainClassNameAndAdditionalArguments(): List<String> {
         return listOf(
+            "-Drobolectric.dependency.repo.url=https://cache-redirector.jetbrains.com/maven-central",
             JUnitCore::class.java.name,
             JUNIT_GENERATED_TEST_CLASS_FQNAME
         )
